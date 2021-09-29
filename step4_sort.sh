12bae02d268c025a03c2fdaf703dbbfdf626f780
Yu <34682491+icanccwhite@users.noreply.github.com>
2021-09-29 12:29:04 +0200

    Create step4_sort.sh

---
+++ step4_sort.sh
@@ -0,0 +1,5 @@
+awk '{print $10}' filtered_gencode.gtf|sed "s/\.[0-9]\+//g" - >new.gtf 
+/share/app/bedtools/bin/bedtools bamtobed -i subbam/5.bam >5.bed
+sort -k1,1 -k2,2n 5.bed > 5.sort.bed
+/share/app/bedtools/bin/bedtools map -s -c 3 -o collapse -a hg38.gene.gff -b 5.sort.bed >end.anno
+awk '{print $(NF)}' end.anno | paste 5.anno - >5_all.anno
